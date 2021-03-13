40d277938a052252cd314074d444bf51a34ccbd4
bantamer <pazhitnykhpetr@gmail.com>
2021-03-13 16:11:01 +0300

    init: setup repo

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['prettier', 'eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    eqeqeq: 'error',
+  },
+};
